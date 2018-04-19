f4818179a500c27e7f3ae98816b944a7c2c6a988
amindap <35238626+amindap@users.noreply.github.com>
2018-04-18 20:29:10 -0600

    Add files via upload

---
+++ Leaderboard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+    public class Leaderboard : MonoBehaviour
+    {
+
+        public Text[] highScores;
+        int[] highScoreValues;
+
+        int nScore;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            highScoreValues = new int[highScores.Length];
+            for (int x = 0; x < highScores.Length; x++)
+            {
+                highScoreValues[x] = PlayerPrefs.GetInt("highScoreValues" + x);
+            }
+            DrawScores();
+
+        }
+        void SaveScores()
+        {
+            for (int x = 0; x < highScores.Length; x++)
+            {
+                PlayerPrefs.SetInt("highScoreValues" + x, highScoreValues[x]);
+            }
+        }
+
+        public void CheckForHighScore(int nScore)
+        {
+            for (int x = 0; x < highScores.Length; x++)
+            {
+                if (nScore> highScoreValues[x])
+                {
+                    for (int y = highScores.Length - 1; y > x; y--)
+                    {
+                        highScoreValues[y] = highScoreValues[y - 1];
+                    }
+                    highScoreValues[x] = nScore;
+                    DrawScores();
+                    SaveScores();
+                    break;
+                }
+            }
+        }
+        void DrawScores()
+        {
+            for (int x = 0; x < highScores.Length; x++)
+            {
+                highScores[x].text = highScoreValues[x].ToString();
+            }
+        }
+    // Update is called once per frame
+    void Update()   
+        {
+        nScore = PlayerPrefs.GetInt("nScore");
+        }
+    }
+
